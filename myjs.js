4d2b300f7b8cd6c18b931bb5d86eee18e0949471
Deepak Kumar <deepak.kumar07@nagarro.com>
2022-08-08 19:33:03 +0530

    first commit

---
+++ myjs.js
@@ -0,0 +1,43 @@
+// console.log('hello world from external file..')
+
+
+
+// DOM - Document Object Model
+// DOM Events
+
+// console.log(window.document);
+// console.log(document);
+
+
+
+// selection of element
+// getElementById
+// getElementsByClassname
+// getElementsByTagName
+// querySelector
+// QuerySelectorAll
+
+function clickFunction () {
+    console.log('button clicked....');
+    var temp = document.getElementById('name').value;
+    console.log(temp);
+    var temp = document.getElementById('email').value;
+    console.log(temp);
+
+    //store in table
+}
+
+
+// picking value from checkbox/radio
+
+// create element
+
+// set attribute
+
+// feed data in dom
+
+// append element
+
+// td - fill td - tr - append to tr - append to table
+
+
